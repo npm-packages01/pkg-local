126257ebc1970e558967023268962044aab075e4
SoraKumo <info@croud.jp>
2019-11-23 11:16:47 +0900

    ディレクトリが無かったら作成するように変更

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,13 @@ if (process.argv.length < 3) {
   process.exit(0);
 }
 try {
+  //パッケージの読み出し
   const pkg = JSON.parse(fs.readFileSync("package.json").toString()) as {
     main?: string;
     types?: string;
     bin?: { [key: string]: string };
   };
+  //ディレクトリのコンバート
   if (pkg.main) pkg.main = path.relative(process.argv[2], pkg.main);
   if (pkg.types) pkg.types = path.relative(process.argv[2], pkg.types);
   if (pkg.bin) {
@@ -19,7 +21,11 @@ try {
       bin[key] = path.relative(process.argv[2], value);
       pkg.bin = bin;
   }
-
+  //ディレクトリの作成
+  if (!fs.existsSync(process.argv[2])) {
+    fs.mkdirSync(process.argv[2]);
+  }
+  //パッケージ内容の変換
   fs.writeFileSync(
     path.resolve(process.argv[2], "package.json"),
     JSON.stringify(pkg, null, "  ")
